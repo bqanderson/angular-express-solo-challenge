1ddbdf5a81e3ec8525f4cc59163939d7c59e90af
bqanderson <bqanderson@me.com>
2016-07-12 19:16:38 -0500

    Client side full functionality & style

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,53 @@
 var app = angular.module('olympicsApp', []);
 
-app.Controller('AthleteController', function($scope, $http){
+app.controller('AthleteController', function($scope, $http){
 
+  $scope.clicked = false;
+
+  $scope.buildList = function() {
+    $http.get('/olympics/badminton').then(function(response){
+      $scope.badminton = response.data;
+      console.log('Success', response);
+    }, function (response) {
+      console.log('Failure', response);
+
+    })
+
+    $http.get('/olympics/fencing').then(function(response){
+      $scope.fencing = response.data;
+      console.log('Success', response);
+    }, function (response) {
+      console.log('Failure', response);
+
+    })
+
+    $http.get('/olympics/equestrian').then(function(response){
+      $scope.equestrian = response.data;
+      console.log('Success', response);
+    }, function (response) {
+      console.log('Failure', response);
+
+    })
+
+    $http.get('/olympics/archery').then(function(response){
+      $scope.archery = response.data;
+      console.log('Success', response);
+    }, function (response) {
+      console.log('Failure', response);
+
+    })
+
+    $http.get('/olympics/sailing').then(function(response){
+      $scope.sailing = response.data;
+      console.log('Success', response);
+    }, function (response) {
+      console.log('Failure', response);
+
+    })
+
+    $scope.clicked = true;
+
+  }
 
 
 });
